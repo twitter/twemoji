2501eaa1aca1f1793e1171ee98aff30f2529e213
Andrea Giammarchi <agiammarchi@twitter.com>
2014-11-13 10:48:03 +0000

    optimized SVGs size + added responsive viewport

--- svg.sh
+++ svg.sh
@@ -1,7 +1,33 @@
-for d in assets/*.ai; do
-  svg=$(echo "$d" | sed 's/.ai/.svg/')
-  echo "creating $svg ..."
-  inkscape -f "$d" -l "$svg"
-done
-mkdir -p svg
-mv assets/*.svg svg/
+createsvg() {
+  local d
+  local svg
+  for d in assets/*.ai; do
+    svg=$(echo "$d" | sed 's/.ai/.svg/')
+    echo "creating $svg ..."
+    inkscape -f "$d" -l "$svg"
+  done
+  mkdir -p svg
+  mv assets/*.svg svg/
+}
+
+viewport() {
+  local f
+  for f in svg/*.svg; do
+    content=$(
+      cat "$f" |
+      sed 's/<!-- Created with Inkscape (http:\/\/www.inkscape.org\/) -->//' |
+      sed 's/width="47.5"/viewBox="0 0 47.5 47.5"/' |
+      sed 's/height="47.5"/style="enable-background:new 0 0 47.5 47.5;"/' |
+      sed -e ':a' -e 'N' -e '$!ba' -e 's/\n//g' |
+      sed -e 's/  */ /g' |
+      sed -e 's/ \/>/\/>/g'
+    )
+    echo "$f"
+    echo "$content" >"$f"
+    
+  done
+}
+
+createsvg
+
+viewport
