fb23d9162e20884b35eb0eedda8840bfd2168104
teeoo <1982890538@qq.com>
2019-03-12 10:56:03 +0800

    Fix #340 Support Typescript, add twemoji.d.ts

---
+++ twemoji.d.ts
@@ -0,0 +1,58 @@
+export const base: string;
+
+export const className: string;
+
+export const ext: string;
+
+export const size: string;
+
+export function onerror(): void;
+
+export function parse(what: any, how?: object): any;
+
+export function replace(text: any, callback: any): any;
+
+export function test(text: any): any;
+
+export namespace convert {
+    function fromCodePoint(codepoint: any): any;
+
+    function toCodePoint(unicodeSurrogates: any, sep: any): any;
+
+    namespace fromCodePoint {
+        const prototype: {
+        };
+
+    }
+
+    namespace toCodePoint {
+        const prototype: {
+        };
+
+    }
+
+}
+
+export namespace onerror {
+    const prototype: {
+    };
+
+}
+
+export namespace parse {
+    const prototype: {
+    };
+
+}
+
+export namespace replace {
+    const prototype: {
+    };
+
+}
+
+export namespace test {
+    const prototype: {
+    };
+
+}
