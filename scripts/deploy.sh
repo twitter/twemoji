feee6da8b1651b9189746b1a61f0a7bc96d4d9cf
Alan Velasco <AlanVelasco.A@gmail.com>
2019-06-25 19:24:28 -0700

    Added deploy script to push built files to gh-pages

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+ROOT=$(
+  cd $(dirname $0)/..
+  /bin/pwd
+)
+OUTPUT="$ROOT/dist/"
+PUBLISH_BRANCH=$1
+VERSION=$2
+
+git fetch --all
+git add -f $OUT
+git checkout $PUBLISH_BRANCH
+git pull origin $PUBLISH_BRANCH
+git mv -f $OUT $VERSION
+git commit -m "Update the Twemoji project and push to $PUBLISH_BRANCH"
+git push origin $PUBLISH_BRANCH
