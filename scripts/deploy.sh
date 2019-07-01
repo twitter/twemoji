2e72acdb16fd68d5a11266fa607d1d3a08bf9be2
Nathan Downs <ndowns@twitter.com>
2019-07-01 15:59:33 -0700

    Stash version changes before switching branches to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,12 @@ PUBLISH_BRANCH=$1
 VERSION=$(cat package.json | jq -r .version)
 
 git fetch --all
+git stash
+
 git checkout $PUBLISH_BRANCH
 git pull origin $PUBLISH_BRANCH
-cd "v/"
+
+pushd "v/"
 # If the folder already exists we want to repalce it
 if [ -d $VERSION ]; then
   rm -r $VERSION
@@ -22,6 +25,8 @@ mv -f $DIST $VERSION
 git add $VERSION
 git commit -q -m "Publish v$VERSION"
 git push origin $PUBLISH_BRANCH
-# Return to your working branch
+popd
+
 git checkout -
-cd $ROOT
+
+git stash apply
